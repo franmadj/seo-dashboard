52747098482b0fc1a3f0f004b3c07f6a10d5c11e
Francisco Mauri <labrest03@gmail.com>
2019-04-04 01:43:09 +0200

    chat css integration

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -75,5 +75,18 @@
             @yield('content')
         </main>
     </div>
+    <button class="open-button" onclick="document.getElementById("myForm").style.display = "block";">Chat</button>
+
+    <div class="chat-popup" id="myForm">
+      <form action="/action_page.php" class="form-container">
+        <h1>Chat</h1>
+
+        <label for="msg"><b>Message</b></label>
+        <textarea placeholder="Type message.." name="msg" required></textarea>
+
+        <button type="submit" class="btn">Send</button>
+        <button type="button" class="btn cancel" onclick="document.getElementById("myForm").style.display = "none";">Close</button>
+      </form>
+    </div>
 </body>
 </html>
